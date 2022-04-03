99605108806ab6ba357f71c07c8d02dd478005a0
Natalia Shuster <shuster.nata@gmail.com>
2022-04-03 15:53:25 +0200

    text comment

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -8,6 +8,7 @@ public class Solution {
     public static void main(String[] args) {
         String quote = "Я никогда не буду работать за копейки. Амиго";
         //напишите тут ваш код
+        //Testing git
         int n = 0;
         while (n < 100) {
             System.out.println(quote);
