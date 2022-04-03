b34ef4c4a8351273dd02275599893ad14d1b73ab
Natalia Shuster <shuster.nata@gmail.com>
2022-04-03 15:23:58 +0200

    initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        //напишите тут ваш код
+        int n = 0;
+        while (n < 100) {
+            System.out.println(quote);
+            n++;
+            
+        }
+        
+
+    }
+}
